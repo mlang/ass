c7c2c2bcb6d186f7448ff7e94ff7b1b96080aaa4
Mario Lang <mlang@blind.guru>
2024-03-05 21:46:08 +0100

    Add some one-shot learning in the form of examples to pacify dialogs model validation errors

--- ass/tools/dialogs.py
+++ ass/tools/dialogs.py
@@ -7,11 +7,13 @@ from ass.tools import function
 
 class ConfirmDialog(BaseModel):
     """A message dialog which asks the user to either confirm or deny.
-    Return type is a boolean value.  Use this for "Yes/No" questions.
+    Return type is a boolean value.
     """
 
     type: Literal['ConfirmDialog']
-    title: str = Field(description="The title of the dialog box.")
+    title: str = Field(max_length=64,
+        description="The title of the dialog box."
+    )
     text: str = Field(
         description="The message text to display in the dialog box."
     )
@@ -24,56 +26,60 @@ class ConfirmDialog(BaseModel):
 
 class TextInputDialog(BaseModel):
     """A dialog which allows the user to enter a string.
-    Return type is either a string or null, which indicates the user cancelled
+    Returns either a string or null, which indicates the user cancelled
     the dialog.
     """
 
     type: Literal['TextInputDialog']
-    title: str
-    text: str
-    ok_label: str = "OK"
-    cancel_label: str = "Cancel"
-
-
-class RadioListDialog(BaseModel):
-    """A single select list dialog.  Use this when querying the user for a
-    single choice from a list of items.
-    Return the selected item, or null when the user cancelled the dialog.
-    """
-    type: Literal['RadioListDialog']
-    title: str = Field(description="The title of the dialog box.")
-    text: str = Field(description="Text displayed above of the radio button list.")
-    values: List[str] = Field(
-        min_length=2,
-        max_length=32,
-        description="The list items."
+    title: str = Field(max_length=64)
+    text: str = Field(
+        description="A message displayed above the text entry field."
     )
     ok_label: str = "OK"
     cancel_label: str = "Cancel"
 
 
+class RadioListDialog(BaseModel):
+    """A radio button list dialog.
+    Returns the selected item, or null when the user cancelled the dialog.
+    """
+
+    type: Literal['RadioListDialog']
+    title: str = Field(max_length=64)
+    text: str = Field(
+        description="Text displayed above of the radio button list."
+    )
+    values: List[str] = Field(
+        min_length=2,
+        max_length=32,
+        description="The list items."
+    )
+    ok_label: str = Field("OK", min_length=1, max_length=20)
+    cancel_label: str = Field("Cancel", min_length=1, max_length=20)
+
+
 class CheckboxListDialog(BaseModel):
-    """A multiselect list dialog.
-    Use this when querying the user for several selections.
-    Return type is a list of all selected items,
+    """A checkbox list dialog.
+    Returns a list of all selected items,
     or null when the user cancelled the dialog.
     """
     type: Literal['CheckboxListDialog']
-    title: str = Field(description="The title of the dialog box.")
+    title: str = Field(max_length=64)
     text: str = Field(description="Text displayed above of the checkbox list.")
     values: List[str] = Field(
         min_length=2,
         max_length=32,
         description="The checkbox items."
     )
-    ok_label: str = "OK"
-    cancel_label: str = "Cancel"
+    ok_label: str = Field("OK", min_length=1, max_length=20)
+    cancel_label: str = Field("Cancel", min_length=1, max_length=20)
 
 
 @function("Allow the model to pop up dialog boxes.")
 class dialogs(BaseModel):
-    """Execute a sequence of dialogs.
+    """Execute a sequence of modal dialogs.
     Choose amongst the available dialog box types approriately.
+    Use this to inquire information from the user or when asked to execute/generate quizzes.
     """
 
     questions: List[ ConfirmDialog
@@ -81,9 +87,36 @@ class dialogs(BaseModel):
                    | CheckboxListDialog
                    | TextInputDialog
                    ] = Field(
-        description="List of modal dialogs to pop up in sequence.  Returns a list of the results.",
+        description="List of modal dialogs to pop up in sequence.  Returns a list of the results. Make sure to set the discriminator 'type' of each dialog correctly.",
         min_length=1, max_length=99,
         discriminator='type'
+    )
+
+    model_config = dict(
+        json_schema_extra=dict(
+            examples=[
+                dict(
+                    questions=[
+                        dict(type='ConfirmDialog', title="Ready?",
+                             text="Are you ready to begin?",
+                             yes_label="Sure", no_label="No thanks"
+                        ),
+                        dict(type='TextInputDialog', title="Your name",
+                             text="Please enter your name."
+                        ),
+                        dict(type='RadioListDialog', title="Gender",
+                             text="Please select your biological gender.",
+                             values=["Female", "Male"]
+                        ),
+                        dict(type='CheckboxListDialog',
+                            title="Favourite colours",
+                            text="What colours do you like?",
+                            values=["Blue", "Green", "Red", "Yellow"]
+                        )
+                    ]
+                )
+            ]
+        )
     )
 
     async def __call__(self, show_dialog, client):
