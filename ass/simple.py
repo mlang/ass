8d36d7ac30e890c90f0d9523f3b1fc4c045cb83a
Mario Lang <mlang@blind.guru>
2024-03-17 23:25:07 +0100

    wip

--- ass/simple.py
+++ ass/simple.py
@@ -1,35 +1,17 @@
-from asyncio import create_task, gather, run, sleep
-from dataclasses import dataclass, field
+from asyncio import run
 from functools import partial
-import re
-from typing import Optional
+
 from click import command, option, argument, pass_obj, File
-from openai import AsyncOpenAI
-from openai.resources.beta.threads.runs import AsyncRuns
-from prompt_toolkit.application import Application
-from prompt_toolkit.application.current import get_app
-from prompt_toolkit.completion import PathCompleter, WordCompleter
-from prompt_toolkit.document import Document
-from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.key_binding.bindings.focus import focus_next
-from prompt_toolkit.layout.containers import (
-    Float, FloatContainer, FormattedTextControl, HSplit, VSplit, Window, WindowAlign
-)
-from prompt_toolkit.layout.layout import Layout
-from prompt_toolkit.lexers import PygmentsLexer
-from prompt_toolkit.styles import Style
-from prompt_toolkit.widgets import (
-    Button, Dialog, FormattedTextToolbar, SearchToolbar, TextArea
-)
-from pygments.lexers.markup import MarkdownLexer
 
 from ass.oai import new_assistant, new_thread, new_files, stream_a_run, MsgText
 from ass.tools import tools_options, tools, tool_call
 
+
 @command(help="Ask a single question")
 @option("--instructions", show_default=True, default="You are a helpful assistant.")
 @option("--model", default="gpt-4-0125-preview", show_default=True)
-@option("--message-file", type=File('r'), default='-')
+@option("--message-file", type=File('r'), default='-', show_default=True,
+        help="File to read the question from.")
 @tools_options
 @argument("files", nargs=-1, type=File('rb'))
 @pass_obj
